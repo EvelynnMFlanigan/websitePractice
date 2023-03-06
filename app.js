3e394bebb16aa079143bbf76bdf83a4775eac706
Freya <flaners2@gmail.com>
2023-03-06 12:42:59 -0500

    index renders "hello world", ready for editing

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const {render} = require('ejs')
 
 const app = express();
 
 let port = '3000';
 let host = 'localhost';
 
+app.set('view engine', 'ejs')
+
 app.listen(port, host , () => {
   console.log('App is listening on port' + port);
 })
+
+app.get('/', (req, res) => {
+res.render('index');
+});
