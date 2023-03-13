f87394344cca45e4304961f24c1182a3ba26c7d4
Freya <flaners2@gmail.com>
2023-03-13 14:47:59 -0400

    successfull test of bootstrap

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const {render} = require('ejs')
 
 const app = express();
@@ -7,6 +8,14 @@ let port = '3000';
 let host = 'localhost';
 
 app.set('view engine', 'ejs')
+
+// usess static middleware to enable Bootstrap
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
 
 app.listen(port, host , () => {
   console.log('App is listening on port' + port);
